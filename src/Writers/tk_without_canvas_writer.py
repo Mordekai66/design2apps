e78b447db9c35ae190b4c8a4d7ad961d79a05093
Abdelrahman <151108921+Mordekai66@users.noreply.github.com>
2025-09-30 19:59:28 +0300

    Delete src directory

--- src/Writers/tk_without_canvas_writer.py
+++
@@ -1,9 +0,0 @@
-def write(line, output_file):
-    if output_file:
-        with open(output_file, 'a', encoding="utf-8") as file:
-            if line == "close()":
-                file.close()
-            else:
-                file.write(line)
-    else:
-        print("No output file specified. Line not written.") 
